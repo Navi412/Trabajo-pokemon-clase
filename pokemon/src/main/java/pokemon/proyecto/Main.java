5f1a613c053a39aa4d463619483ce2fdbabc7cbd
Navi <56206451+ivanchuppp@users.noreply.github.com>
2025-04-30 11:29:34 +0200

    Update Main.java

--- pokemon/src/main/java/pokemon/proyecto/Main.java
+++ pokemon/src/main/java/pokemon/proyecto/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static int cliricomblins = 0; // Cambiado de "dinero" a "cliricomblins"
+    public static int cliricomblins = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
